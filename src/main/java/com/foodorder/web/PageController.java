4d27676d20616a74e6f7a17563034bea2eb19b5f
hieu170120 <mrhaibigtwo@github.com>
2026-04-05 20:45:12 +0700

    add: basic UI for dev

---
+++ src/main/java/com/foodorder/web/PageController.java
@@ -0,0 +1,23 @@
+package com.foodorder.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String landing() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
